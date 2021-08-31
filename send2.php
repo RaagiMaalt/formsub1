78731f6ca545e42b2dce21b717eaa92075c83fdc
RaagiMaalt <89734855+RaagiMaalt@users.noreply.github.com>
2021-08-31 07:19:31 +0530

    Update send2.php

--- send2.php
+++ send2.php
@@ -2,18 +2,14 @@
 
 #Receive user input
 $email_address = $_POST['email_address'];
+echo $email_address;
 $feedback = $_POST['feedback'];
+echo $feedback;
 
-#Filter user input
-function filter_email_header($form_field) {  
-return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
-}
-
-$email_address  = filter_email_header($email_address);
 
 #Send email
 $headers = "From: $email_address";
-echo $email_address . $feedback . $headers ;
+echo  $headers ;
 
 $sent = mail('ssy.balu@gmail.com', 'Feedback Form Submission', $feedback, $headers);
 
